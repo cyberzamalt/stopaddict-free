afc412ef78a130570f57d8e8b1c58d1888313448
cyberzamalt <abriounicolas@gmail.com>
2025-12-25 20:11:14 +0100

    Update CalendrierFragment.kt

--- app/src/main/java/com/stopaddict/CalendrierFragment.kt
+++ app/src/main/java/com/stopaddict/CalendrierFragment.kt
@@ -2,7 +2,6 @@
     
     import android.graphics.Color
     import android.os.Bundle
-    import android.util.Log
     import android.view.LayoutInflater
     import android.view.View
     import android.view.ViewGroup
